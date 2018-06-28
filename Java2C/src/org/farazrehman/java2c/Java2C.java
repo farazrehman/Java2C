d4f6df86b30d40059d3bb63627b3840feb749a8e
farazrehman <farazrehman1@gmail.com>
2018-06-27 22:57:37 -0700

    Initial checkin

---
+++ Java2C/src/org/farazrehman/java2c/Java2C.java
@@ -0,0 +1,19 @@
+package org.farazrehman.java2c;
+
+public class Java2C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("---- HELLO 2C ----");
+		System.out.println(getName());
+
+	}
+	
+	static String getName () {
+		String name = "Static Mutable String";
+		return name;
+	}
+	
+
+}
